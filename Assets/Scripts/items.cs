fdf4cd46958efe6fc629ae320d62e781af783a34
Juan Alfredo <38137911+alfredojuareztamayo@users.noreply.github.com>
2024-07-30 21:55:08 -0600

    Update items.cs
    summary

--- Assets/Scripts/items.cs
+++ Assets/Scripts/items.cs
@@ -6,66 +6,73 @@ using System;
 
 public class items : MonoBehaviour
 {
-    public string mensajeMQTT;
-    private MQTTManager mqttManager;
-    public bool goodBad = true;
-    private ManagerWin ManagerWin;
-    //public Color newColor;
+   public string mensajeMQTT; // MQTT message
+private MQTTManager mqttManager; // Reference to MQTTManager
+public bool goodBad = true; // Flag to determine good or bad item
+private ManagerWin ManagerWin; // Reference to ManagerWin
 
-
-    private void Start()
+private void Start()
+{
+    // Get the Renderer component and change the material color to a random color
+    Renderer renderer = GetComponent<Renderer>();
+    if (renderer != null)
     {
-        Renderer renderer = GetComponent<Renderer>();
-        if (renderer != null)
-        {
-            // Cambiar el color del material a uno aleatorio
-            Color randomColor = GetRandomColor();
-            renderer.material.color = randomColor;
-            Debug.Log("Color changed to: " + randomColor);
-        }
-        else
-        {
-            Debug.LogError("Renderer not found on the GameObject");
-        }
-
-        mqttManager = FindObjectOfType<MQTTManager>();
-        if (mqttManager == null)
-        {
-            Debug.LogError("MQTTManager not found in the scene");
-        }
-        ManagerWin = FindObjectOfType<ManagerWin>();
-        if (ManagerWin == null)
-        {
-            Debug.LogError("ManagerWin not found in the scene");
-        }
-
-
+        Color randomColor = GetRandomColor();
+        renderer.material.color = randomColor;
+        Debug.Log("Color changed to: " + randomColor);
+    }
+    else
+    {
+        Debug.LogError("Renderer not found on the GameObject");
     }
 
-     private void OnTriggerEnter(Collider other)
-     {
-         if (other.gameObject.CompareTag("Player"))
-         {
-            if (mqttManager != null)
-            {
-                mqttManager.SendCommand(mensajeMQTT);
-                Debug.Log("Item picked up and command sent");
-                
-            }
-            if(ManagerWin != null && goodBad)
-            {
-                ManagerWin.RestValueOfItems();
-            }
-            if(!goodBad)
-            {
-                ManagerWin.RestLifePlayer();
-            }
-            gameObject.SetActive(false);
-        }
-     }
-    private Color GetRandomColor()
+    // Find MQTTManager in the scene
+    mqttManager = FindObjectOfType<MQTTManager>();
+    if (mqttManager == null)
     {
-        // Generar un color aleatorio
-        return new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+        Debug.LogError("MQTTManager not found in the scene");
     }
+
+    // Find ManagerWin in the scene
+    ManagerWin = FindObjectOfType<ManagerWin>();
+    if (ManagerWin == null)
+    {
+        Debug.LogError("ManagerWin not found in the scene");
+    }
+}
+
+private void OnTriggerEnter(Collider other)
+{
+    // Check if the colliding object has the "Player" tag
+    if (other.gameObject.CompareTag("Player"))
+    {
+        if (mqttManager != null)
+        {
+            // Send MQTT command
+            mqttManager.SendCommand(mensajeMQTT);
+            Debug.Log("Item picked up and command sent");
+        }
+
+        if (ManagerWin != null && goodBad)
+        {
+            // Reduce the value of items if it's a good item
+            ManagerWin.RestValueOfItems();
+        }
+
+        if (!goodBad)
+        {
+            // Reduce player's life if it's a bad item
+            ManagerWin.RestLifePlayer();
+        }
+
+        // Deactivate the game object
+        gameObject.SetActive(false);
+    }
+}
+
+private Color GetRandomColor()
+{
+    // Generate a random color
+    return new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+}
 }
